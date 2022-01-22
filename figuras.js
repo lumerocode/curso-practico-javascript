0026395147c58be9beaf388e18250b37888b6e7b
Luis Manuel Meléndez Rodriguez <luismelendez0894@gmail.com>
2022-01-21 21:06:32 -0500

    commit-figura-cuadrado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+//Código del Cuadrado
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado +"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado +"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado +"cm2");
+
+//Código del Triángulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+
+
+console.log("Los lados del cuadrado miden: " + ladoCuadrado +"cm");
