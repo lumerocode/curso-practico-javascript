93cff4c3ccb1c4e60d25609688a92b5aed8e42d1
Luis Manuel Meléndez Rodriguez <luismelendez0894@gmail.com>
2022-01-22 12:36:27 -0500

    commit-areas-perimetros

--- figuras.js
+++ figuras.js
@@ -1,18 +1,88 @@
 //Código del Cuadrado
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado +"cm");
+console.group("Cuadrados");
+    function perimetroCuadrado(lado){
+        return lado * 4;
+    } 
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado +"cm");
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El área del cuadrado es: " + areaCuadrado +"cm2");
+    function areaCuadrado (lado) {  
+        return lado * lado;
+    } 
+console.groupEnd();
 
 //Código del Triángulo
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
+console.group("Triángulos");
+    function perimetroTriangulo (lado1, lado2, base) { 
+        return lado1 + lado2 + base;
+     } 
+
+    function areaTriangulo(base, altura) {
+        return (base * altura) / 2;
+    } 
+console.groupEnd();
+
+//Código del Circulo
+console.group("Circulos");
+    function diametroCirculo(radio) { 
+        return radio * 2;
+     }
+
+    const PI = Math.PI; 
+
+    function perimetroCirculo(diametro) {
+        return diametro * PI;
+    } 
+
+    function areaCirculo(radio) { 
+        return (radio * radio) / 2;
+     }  
+console.groupEnd();
 
 
 
-console.log("Los lados del cuadrado miden: " + ladoCuadrado +"cm");
+//Aquí interactuamos con el HTML
+
+//CUADRADO
+function calcularPerimetroCuadrado() { 
+    const input = document.getElementById("InputCuadrado"); //obtenemos todo el input
+    const value = input.value; // aqui guardamos solo el valor que necesitamos
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+ }
+
+ function calcularAreaCuadrado() { 
+    const input = document.getElementById("InputCuadrado"); //obtenemos todo el input
+    const value = input.value; // aqui guardamos solo el valor que necesitamos
+
+    const area = areaCuadrado(value);
+    alert(area);
+  }
+
+  
+//TRIANGULO
+
+function calcularPerimetroTriangulo() { 
+    const input1 = document.getElementById("InputTrianguloLado1"); 
+    const value1 = input1.value;
+
+    const input2 = document.getElementById("InputTrianguloLado2"); 
+    const value2 = input2.value;
+
+    const input3 = document.getElementById("InputTrianguloBase"); 
+    const value3 = input3.value;
+
+    const perimetro = perimetroTriangulo(value1, value2, value3);
+    alert(perimetro);
+ }
+
+ function calcularAreaTriangulo() {
+    const input3 = document.getElementById("InputTrianguloBase"); 
+    const value3 = input3.value;
+
+    const input4 = document.getElementById("InputTrianguloAltura"); 
+    const value4 = input4.value;
+
+    const area = areaTriangulo(value3, value4);
+    alert(area);
+  }
+
